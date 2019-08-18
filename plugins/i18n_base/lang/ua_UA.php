b05922a929bde9d0b6ec36237ee20da6be67d026
Bogdan <Kleindberg@users.noreply.github.com>
2019-08-18 12:18:47 +0300

    Create ua_UA.php
    Ukrainian translation for i18n_base plugin.

---
+++ plugins/i18n_base/lang/ua_UA.php
@@ -0,0 +1,17 @@
+<?php
+$i18n = array(
+    'CLOSE_ALL' => "Тільки батьківські сторінки",
+    'DEFAULT_LANGUAGE_DESCR' => "Мова для сторінок без префіксів (по типу <strong>index</strong>), наприклад <strong>ua</strong>:",
+    'FILTER' => "Фільтр",
+    'FILTER_TITLE' => "Вкажіть частину заголовку або : і тег/ключове слово.",
+    'NEW_LANGUAGE_DESCR' => "Для перекладу створіть сторінку, URL котрої буде містити код мови (наприклад, <strong>index_en</strong> для англійскої).",
+    'OPEN_ALL' => "Всі сторінки",
+    'PAGES' => "Всі сторінки (I18N)",
+    'SAVE_DEFAULT_LANGUAGE' => "Зберегти",
+    'SAVE_FAILURE' => "<b>Помилка:</b> Невірний код мови. Використовуйте такі коди: <strong>en</strong> - англійска, <strong>ru</strong> - російська тощо.",
+    'SAVE_OPEN' => "Зберегти поточний список сторінок",
+    'SAVE_SUCCESS' => "Мова успішно збережена",
+    'VIEW_HIERARCHICAL' => "Ієрархія",
+    'VIEW_TITLE' => "За назвою",
+    'PLUGIN_DESCRIPTION' => "Переклад змісту на основі назв URL"
+);
